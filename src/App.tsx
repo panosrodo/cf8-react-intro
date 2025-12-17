0993afe3c04d443c8813142ae7f005cfe7d44090
panosrodo <brandonroots@hotmail.gr>
2025-12-17 12:53:57 +0200

    feat: add component ArrowFunctionalComponentWithPropsType

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@
 // import ClassComponent from "./components/ClassComponent.tsx";
 // import FunctionalComponent from "./components/FunctionalComponent.tsx";
 // import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
-import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
+// import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
+import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWithPropsType.tsx";
 
 function App() {
   return (
@@ -11,8 +12,12 @@ function App() {
     {/*  <ClassComponent />*/}
     {/*  <FunctionalComponent />*/}
     {/*  <ArrowFunctionalComponent />*/}
-      <ArrowFunctionalComponentWithProps title="Is a Arrow Functional Component With Props" />
-      <ArrowFunctionalComponentWithProps title="2nd Title" />
+    {/*  <ArrowFunctionalComponentWithProps title="Is a Arrow Functional Component With Props" />*/}
+    {/*  <ArrowFunctionalComponentWithProps title="2nd Title" />*/}
+      <ArrowFunctionalComponentWithPropsType
+          title="Is a Arrow Functional Component With 2 Props"
+          description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, veniam."
+      />
     </>
   )
 }
