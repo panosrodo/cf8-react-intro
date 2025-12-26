b97e549a4552522e49c19e62329d54c13fb0fc4a
panosrodo <brandonroots@hotmail.gr>
2025-12-26 13:41:41 +0200

    feat: learning react-routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-// import ViteIntro from "./components/ViteIntro/ViteIntro.tsx";
+// import ViteIntro from "./components/ViteIntro/ViteIntro.tsx"
 // import ClassComponent from "./components/ClassComponent.tsx";
 // import FunctionalComponent from "./components/FunctionalComponent.tsx";
 // import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
@@ -6,37 +6,31 @@
 // import ArrowFunctionalComponentWithPropsType from "./components/ArrowFunctionalComponentWithPropsType.tsx";
 // import Card from "./components/Card.tsx";
 // import Layout from "./components/Layout.tsx";
+// import WindowSize from "./components/WindowSize.tsx";
+// import FocusInput from "./components/FocusInput.tsx";
+// import PreviewsValue from "./components/PreviousValue.tsx";
 // import CounterWithRef from "./components/CounterWithRef.tsx";
 // import {useEffect} from "react";
-// import PreviousValues from "./components/PreviousValues.tsx";
 // import FocusInput from "./components/FocusInput.tsx";
-// import WindowSize from "./components/WindowSize.tsx";
-// import CounterAdvanced from "./components/CounterAdvanced.tsx";
-// import CounterWithCustomHook from "./components/CounterWithCustomHook.tsx";
-// import NameChangerWithEffect from "./components/NameChangeWithEffects.tsx";
-// import Timer from "./components/Timer.tsx";
-// import {useEffect} from "react";
+// import ViteIntro from "./components/ViteIntro/ViteIntro.tsx";
 // import FunctionalComponentWithState from "./components/FunctionalComponentWithState.tsx";
 // import ClassComponentWithState from "./components/ClassComponentWithState.tsx";
 // import Counter from "./components/Counter.tsx";
 // import NameChanger from "./components/NameChanger.tsx";
 // import CounterWithMoreStates from "./components/CounterWithMoreStates.tsx";
+// import CounterAdvanced from "./components/CounterAdvanced.tsx";
+// import CounterWithCustomHook from "./components/CounterWithCustomHook.tsx";
+// import NameChangerWithEffect from "./components/NameChangerWithEffect.tsx";
 // import {useEffect} from "react";
-import {BrowserRouter, Route, Routes} from "react-router"
+// import Timer from "./components/Timer.tsx";
+import { BrowserRouter, Routes, Route } from "react-router";
 import NameChanger from "./components/NameChanger.tsx";
 import HomePage from "./pages/HomePage.tsx";
 import Timer from "./components/Timer.tsx";
+// import Layout from "./components/Layout.tsx";
+import RouterLayout from "./components/RouterLayout.tsx";
 
 function App() {
-
-    // useEffect(setup,[]);
-    //
-    // useEffect(() => {
-    //     // setup function
-    //     return () => {
-    //         // cleanup
-    //     }
-    // }, [dep]);
 
     // useEffect(() => {
     //   const id  = setInterval(()=> console.log(id),1000)
@@ -52,64 +46,88 @@ function App() {
     // }, [])
 
     // useEffect(() => {
-    //     history.pushState({page: 1}, "", "/page");
-    //     history.replaceState({page: 1}, "", "/page1");
+    //   history.pushState({page: 1}, "", "/page");
+    //   history.replaceState({page: 1}, "", "/page1");
     //
-    //     window.onpopstate = (e) => {
-    //         console.log(e.state);
-    //     }
+    //   window.onpopstate = (e) => {
+    //     console.log(e.state);
+    //   }
     // })
 
-  return (
-    <>
-    {/*<ViteIntro />*/}
-    {/*  <ClassComponent />*/}
-    {/*  <FunctionalComponent />*/}
-    {/*  <ArrowFunctionalComponent />*/}
-    {/*  <ArrowFunctionalComponentWithProps title="Is a Arrow Functional Component With Props" />*/}
-    {/*  <ArrowFunctionalComponentWithProps title="2nd Title" />*/}
-    {/*  <ArrowFunctionalComponentWithPropsType*/}
-    {/*      title="Is a Arrow Functional Component With 2 Props"*/}
-    {/*      description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, veniam."*/}
-    {/*  />*/}
-    {/*    <Card title="Card">*/}
-    {/*      <ArrowFunctionalComponentWithPropsType*/}
-    {/*        title="Is a Arrow Functional Component With 2 Props"*/}
-    {/*        description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, veniam."*/}
-    {/*      />*/}
-    {/*    </Card>*/}
-    {/*<Layout>*/}
+    return (
+        <>
+            {/*<ViteIntro />*/}
+            {/*<ClassComponent />*/}
+            {/*<FunctionalComponent />*/}
+            {/*<ArrowFunctionalComponent />*/}
+            {/*<ArrowFunctionalComponentWithProps title="Is a Arrow Functional Component With Props" />*/}
+            {/*<ArrowFunctionalComponentWithProps title="2nd Title" />*/}
+            {/*<ArrowFunctionalComponentWithPropsType*/}
+            {/*  title="Is a Arrow Functional Component With 2 Props"*/}
+            {/*  description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, veniam."*/}
+            {/*/>*/}
+            {/*<Card title="Card">*/}
+            {/*  <ArrowFunctionalComponentWithPropsType*/}
+            {/*    title="Is a Arrow Functional Component With 2 Props"*/}
+            {/*    description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, veniam."*/}
+            {/*  />*/}
+            {/*</Card>*/}
 
-        {/*<ArrowFunctionalComponentWithPropsType*/}
-        {/*title="Is a Arrow Functional Component With 2 Props"*/}
-        {/*description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, veniam."*/}
-        {/*/>*/}
-        {/*<FunctionalComponentWithState />*/}
-        {/*<ClassComponentWithState />*/}
-        {/*<Counter />*/}
-        {/*<NameChanger />*/}
-        {/*<CounterWithMoreStates/>*/}
+            {/*<Layout>*/}
+            {/*<ArrowFunctionalComponentWithPropsType*/}
+            {/*  title="Is a Arrow Functional Component With 2 Props"*/}
+            {/*  description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod, veniam."*/}
+            {/*/>*/}
+            {/*<FunctionalComponentWithState />*/}
+            {/*<ClassComponentWithState />*/}
+            {/*<Counter />*/}
+            {/*<NameChanger/>*/}
+            {/*<CounterWithMoreStates/>*/}
+            {/*<CounterAdvanced/>*/}
+            {/*<CounterWithCustomHook/>*/}
+            {/*<NameChangerWithEffect />*/}
+            {/*<Timer/>*/}
+            {/*<WindowSize/>*/}
+            {/*<FocusInput/>*/}
+            {/*<PreviewsValue/>*/}
+            {/*<CounterWithRef />*/}
+            {/*</Layout>*/}
 
-        {/*<CounterAdvanced />*/}
-        {/*<CounterWithCustomHook />*/}
-        {/*<NameChangerWithEffect />*/}
-        {/*<Timer />*/}
-        {/*<WindowSize />*/}
-        {/*<FocusInput />*/}
-        {/*<PreviousValues />*/}
-        {/*<CounterWithRef />*/}
-        {/*</Layout>*/}
+            <BrowserRouter>
+                <Routes>
 
-        <BrowserRouter>
-            <Routes>
-                <Route index element={<HomePage/>} />
-                <Route path="name-changer" element={<NameChanger />} />
-                <Route path="timer" element={<Timer />} />
-            </Routes>
-        </BrowserRouter>
+                    <Route element={<RouterLayout/>}>
 
-    </>
-  )
+                        <Route index element={<HomePage />} />
+
+                        {/*<Route path="examples/name-changer" element={<NameChanger />} />*/}
+                        {/*<Route path="examples/timer" element={<Timer />} />*/}
+
+                        <Route path="examples?">
+                            <Route path="name-changer" element={<NameChanger/>}/>
+                            <Route path="timer" element={<Timer/>}/>
+                            {/*<Route path="*" element={<ExamplePage/>}/>*/}
+                        </Route>
+
+                    </Route>
+
+                    {/*<Route path="users/:userId" element={<UserPage />} />*/}
+                    {/*<Route path="users/:userId/accounts" element={<UserAccountPage />} />*/}
+
+                    {/*<Route path="users">*/}
+                    {/*  <Route path=":userId">*/}
+                    {/*    <Route index element={<UserPage/>}/>*/}
+                    {/*    <Route path="accounts" element={<UserAccountPage/>}/>*/}
+                    {/*  </Route>*/}
+                    {/*</Route>*/}
+
+                    {/*<Route path="files/*" element={<FilePage />} />*/}
+
+                </Routes>
+            </BrowserRouter>
+
+        </>
+    )
 }
 
 export default App
